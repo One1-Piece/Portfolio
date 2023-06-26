ed99ed63f2411dd9b865d886961d9fd0f8b460fb
Taha <thebestno123@gmail.com>
2023-06-26 13:16:14 +0400

    Next Config Updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: {
-        // Use this option to enable export
-        exportType: 'export',
-      },
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
 }
 
 module.exports = nextConfig
